018ce17216fc34109f7401cd4e02fe9e4f60d3d7
Iago Lucchese <iagolucchese@gmail.com>
2023-12-03 14:10:13 -0300

    fixed sprite order for salesman, added OpenBag input action

--- Assets/Scripts/Utils/InputToUnityEvent.cs
+++ Assets/Scripts/Utils/InputToUnityEvent.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using ClothesShopToy.UI;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
@@ -8,23 +10,34 @@ namespace ClothesShopToy.Utils
     public class InputToUnityEvent : MonoBehaviour
     {
         [SerializeField] private InputActionReference inputAction;
+        [SerializeField] private bool blockedWhenScreenIsOpen;
         [SerializeField] private UnityEvent onAction;
-
+        [ShowNonSerializedField] private bool isBlocked;
+        
         private void OnEnable()
         {
             inputAction.action.Enable();
             inputAction.action.started += StartedCallback;
+
+            ScreenManagerBase.OnScreenOpened += BlockAction;
+            ScreenManagerBase.OnScreenClosed += UnlockAction;
         }
 
         private void OnDisable()
         {
             inputAction.action.started -= StartedCallback;
+            ScreenManagerBase.OnScreenOpened -= BlockAction;
+            ScreenManagerBase.OnScreenClosed -= UnlockAction;
         }
 
         private void StartedCallback(InputAction.CallbackContext callbackContext)
         {
             if (gameObject.activeInHierarchy == false) return;
+            if (blockedWhenScreenIsOpen && isBlocked) return;
             onAction?.Invoke();
         }
+
+        private void BlockAction() => isBlocked = true;
+        private void UnlockAction() => isBlocked = false;
     }
 }
