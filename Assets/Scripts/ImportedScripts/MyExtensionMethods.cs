f1a4373e7270c011dea6fbee16b91747a2d09b9e
Iago Lucchese <iagolucchese@gmail.com>
2023-12-02 22:02:48 -0300

    imported more extension methods

--- Assets/Scripts/ImportedScripts/MyExtensionMethods.cs
+++ Assets/Scripts/ImportedScripts/MyExtensionMethods.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace ImportedScripts
 {
@@ -72,6 +74,186 @@ namespace ImportedScripts
 
             animator.SetFloat(paramHash, value);
             return true;
+        }
+        #endregion
+        
+        #region Collection Extensions
+        /// <summary>
+        /// Checks that the collection is not null and not empty
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns>True if not null and not empty</returns>
+        public static bool IsValidAndNotEmpty(this ICollection collection)
+        {
+            return collection != null && collection.Count > 0;
+        }
+    
+        /// <summary>
+        /// Checks if the collection is null or empty
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns>True if null or empty</returns>
+        public static bool IsInvalidOrEmpty(this ICollection collection)
+        {
+            return collection == null || collection.Count <= 0;
+        }
+        #endregion
+        
+        #region IList Extensions
+        /// <summary>
+        /// Randomizes the list order.
+        /// </summary>
+        /// <param name="list"></param>
+        public static void Shuffle(this IList list)
+        {
+            System.Random rng = new System.Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                (list[k], list[n]) = (list[n], list[k]);
+            }
+        }
+
+        /// <summary>
+        /// Gets a single random element from the list. Uses UnityEngine.Random.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T GetRandom<T>(this IList<T> list) => list[Random.Range(0, list.Count)];
+
+        /// <summary>
+        /// Get a random elements from the list.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="amount">Amount of random elements</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>List of random elements T</returns>
+        public static List<T> GetRandom<T>(this IList<T> list, int amount)
+        {
+            if (amount < 2)
+            {
+                return new List<T> { list.GetRandom() };
+            }
+
+            List<T> results = new List<T>(list);
+            results.Shuffle();
+
+            int trueAmount = amount > results.Count ? results.Count : amount;
+            if (results.Count > trueAmount)
+                results.RemoveRange(trueAmount, results.Count - trueAmount);
+
+            return results;
+        }
+
+        /// <summary>
+        /// Get the last item on the list.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T GetLast<T>(this IList<T> list) => list[list.Count - 1];
+
+        /// <summary>
+        /// Removes all null elements from the list.
+        /// </summary>
+        public static void Sanitize(this IList list)
+        {
+            if (list.IsInvalidOrEmpty()) return;
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                if (list[i] != null) continue;
+                list.RemoveAt(i);
+            }
+        }
+
+        /// <summary>
+        /// Returns element at 'index', using Mathf.Repeat to loop the index value.
+        /// </summary>
+        public static T ElementAtIndexRepeat<T>(this IList<T> list, int index)
+        {
+            if (list == null || list.Count <= 0) return default;
+            int loopedIndex = (int)Mathf.Repeat(index, list.Count);
+            return list[loopedIndex];
+        }
+
+        /// <summary>
+        /// A null-safe version of the Count property. Returns zero if the list is null.
+        /// </summary>
+        public static int SafeCount<T>(this IList<T> list)
+        {
+            return list?.Count ?? 0;
+        }
+
+        /// <summary>
+        /// Adds to list only if the element doesn't exist there yet.
+        /// </summary>
+        /// <returns>True if an element was added, false otherwise</returns>
+        public static bool AddIfNew<T>(this IList<T> list, T newElement)
+        {
+            if (list == null) return false;
+            if (list.Contains(newElement)) return false;
+
+            list.Add(newElement);
+            return true;
+        }
+
+        /// <summary>
+        /// Adds to list all the elements that dont exist there yet.
+        /// </summary>
+        /// <returns>True if an element was added, false otherwise</returns>
+        public static bool AddIfNew<T>(this IList<T> list, IList<T> newElements)
+        {
+            if (list == null) return false;
+            if (newElements == null) return false;
+            if (newElements.Count <= 0) return false;
+
+            bool response = false;
+            foreach (T newElement in newElements)
+            {
+                if (newElement == null) continue;
+                response |= list.AddIfNew(newElement);
+            }
+            return response;
+        }
+
+        /// <summary>
+        /// Moves an element of the list to a new position using its index.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="oldIndex"></param>
+        /// <param name="newIndex"></param>
+        public static void Move<T>(this IList<T> list, int oldIndex, int newIndex)
+        {
+            var item = list[oldIndex];
+            list.RemoveAt(oldIndex);
+            if (newIndex > oldIndex) newIndex--;
+            list.Insert(newIndex, item);
+        }
+
+        /// <summary>
+        /// Finds the element index and then moves it to a new position.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="item"></param>
+        /// <param name="newIndex"></param>
+        public static void Move<T>(this IList<T> list, T item, int newIndex)
+        {
+            if (item != null)
+            {
+                var oldIndex = list.IndexOf(item);
+                if (oldIndex > -1)
+                {
+                    list.RemoveAt(oldIndex);
+                    if (newIndex > oldIndex) newIndex--;
+                    list.Insert(newIndex, item);
+                }
+            }
         }
         #endregion
     }
